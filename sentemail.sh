24c30ab45c7f7c325f41e09b42eae2267b28136d
mcochi <52410138+mcochi@users.noreply.github.com>
2019-11-28 20:38:21 +0100

    Delete sentemail.sh

--- sentemail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-{
-echo To: rmcochi@gmail.com
-echo From: rmcochi@gmail.com
-echo Subject: Camara de Seguridad 
-echo Para ver los videos de la cámara de vigilancia conectarse a rmcochi.ddns.net con FileZilla en el directorio /mnt/video/ Recuerda conectarte primero a la VPN y que los videos se borran tras 48 horas; 
-} | ssmtp coro.dri@hotmail.com jcochimonzon2@gmail.com 
- 
